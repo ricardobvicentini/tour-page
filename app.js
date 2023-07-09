e1e16cec35cb85d2343787366f447f07b0db55c8
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2023-07-09 19:13:54 -0300

    Add faders JS

--- app.js
+++ app.js
@@ -22,9 +22,25 @@ const observer = new IntersectionObserver(entries => {
         entry.target.classList.toggle("show", entry.isIntersecting)
     })
 }, {
-    rootMargin: "0px 0px -100px 0px"
+    rootMargin: "-200px 0px -200px 0px"
 })
 
 hiddenElements.forEach(hidden => {
     observer.observe(hidden)
 })
+
+const faders = document.querySelectorAll('.fade-in');
+
+
+const appearOnScroll = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("appear", entry.isIntersecting)
+    })
+}, {
+    rootMargin: "0px 0px -300px 0px"
+})
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader)
+})
+
