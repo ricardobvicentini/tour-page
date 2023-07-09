ffc7d198ab506c3347eb5a87c23924d015ef0300
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2023-07-09 17:27:43 -0300

    Add new JS

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const observer = new IntersectionObserver((entries) => {
+/* const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
             entry.target.classList.remove('show');
-            /* observer.unobserve(entry.target); */
-        }
+        } 
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+ */
 
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+}, {
+    rootMargin: "0px 0px -100px 0px"
+})
+
+hiddenElements.forEach(hidden => {
+    observer.observe(hidden)
+})
